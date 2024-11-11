96fa032feabcb1435a97f0415ecd8be4a7ac17f3
Benjamin-mne <bernjamin.delgado.elias@gmail.com>
2024-11-11 19:16:14 -0300

    add metadata - data.js traduccion

--- data.js
+++ data.js
@@ -2,16 +2,16 @@ export const projects = [
     {
         id: 0,
         type: 'video',
-        title: "Logotipos en 3D",
-        description: "Visuales para música de Caravana de Carnaval | Comparsa Kamarr 2024 | Chá | Carnaval de Gualeguaychú",
+        title: "3D Logos",
+        description: "Visuals for Caravana de Carnaval music | Comparsa Kamarr 2024 | Chá | Gualeguaychú Carnival",
         src: "video/logos-3d.mp4",
         link: "https://www.youtube.com/watch?v=C2UaAACZT1o&list=RDEM8V5f4KIC6JqgYc0O4amATw&start_radio=1&ab_channel=CaravanadeCarnaval"
     },
     {
         id: 1,
         type: 'video',
-        title: "Ríos Electricos Vol. 2",
-        description: "Diseño tapa y contrapata del disco",
+        title: "Electric Rivers Vol. 2",
+        description: "Album cover and back cover design",
         src: "./video/re-vol-2.mp4",
         link: "https://www.instagram.com/p/C2U4wDZLaQp/?img_index=1"
     },
@@ -19,7 +19,7 @@ export const projects = [
         id: 2,
         type: 'video',
         title: "Chá",
-        description: "Video animado temática Comparsa Kamarr 2024 Carnaval del País",
+        description: "Animated video themed Comparsa Kamarr 2024 Gualeguaychú Carnival",
         src: "video/cha.mp4",
         link: "https://www.instagram.com/reel/C0FvYBurb-i/"
     },
@@ -27,15 +27,15 @@ export const projects = [
         id: 3,
         type: 'video',
         title: "UM 86 - Full Album",
-        description: "Visuales para el album",
+        description: "Visuals for the album",
         src: "video/um-86.mp4",
         link: "https://www.youtube.com/watch?v=aJu1u-cJYIk&ab_channel=NicolasBava"
     },
     {
         id: 4,
         type: 'video',
-        title: "Comparsa Kamarr en 2024 | Chá",
-        description: "Video presentación logo de la temática",
+        title: "Comparsa Kamarr in 2024 | Chá",
+        description: "Logo introduction video for the theme",
         src: "video/presenta-logo.mp4",
         link: "https://www.instagram.com/p/CxlIHsrLEcJ/"
     },
@@ -43,7 +43,7 @@ export const projects = [
         id: 5,
         type: 'video',
         title: "Full Moon - Full Album",
-        description: "Video anuncio fecha de estreno (Reel promoción)",
+        description: "Release date announcement video (Promotional reel)",
         src: "video/full-moon.mp4",
         link: "https://www.instagram.com/p/C8NiBSHplr5"
     }
